93c0876f1a75462d03ff155e22ac2c834143a19a
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-07-13 20:28:46 +1000

    Create placeholder content

---
+++ client/src/components/Home.js
@@ -0,0 +1,15 @@
+// The Home component is the landing page of the application, presenting general information.
+
+
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to StudentDB - the Student Management System</h1>
+      <p>This system allows educational institutions to manage student data efficiently and effectively.</p>
+    </div>
+  );
+}
+
+export default Home;
