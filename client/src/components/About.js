f0b2c4b0b6dbdf318f723bd936bdddf7bbe6f48b
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-07-13 20:30:37 +1000

    Create placeholder content

---
+++ client/src/components/About.js
@@ -0,0 +1,14 @@
+// This component provides information about the student management system, such as its purpose and the team behind it
+
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>The StudentDB, a Student Management System was developed to provide educational institutions with a robust tool for managing student data and enhancing administrative processes.</p>
+    </div>
+  );
+}
+
+export default About;
