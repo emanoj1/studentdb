9f25af598fe3a95ec274090eb1305b379644022f
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-07-14 13:03:40 +1000

    Add the institutions route to the Express server setup

--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 // Import routes
 const authRoutes = require('./routes/auth');
 const studentRoutes = require('./routes/students');
+app.use('/api/institutions', institutionRoutes);
 
 // Route middlewares
 app.use('/api/user', authRoutes);
